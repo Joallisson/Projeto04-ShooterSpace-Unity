8adf09c2fbc64eb3c2e6be355a63a3970b159f05
Joallisson <joallissonnc@gmail.com>
2022-12-30 19:17:49 -0300

    157. Jogador disparando projeteis

---
+++ Projeto 04 Shooter Space/Assets/_Game/Scripts/Player.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private GameObject laserPrefab;
+    [SerializeField] Transform spawnProjectilePosition;
+    [SerializeField] private float firingRate;
+    private float timeToShoot;
+    private GameController gameController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameController = FindObjectOfType<GameController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ShootProjectile();
+    }
+
+    private void ShootProjectile()
+    {
+        timeToShoot += Time.deltaTime;
+
+         if(gameController.shootManual && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began && timeToShoot >= firingRate) //Disparo Manual
+         {
+            GameObject tempProjectile = Instantiate(laserPrefab, spawnProjectilePosition.position, Quaternion.identity) as GameObject;
+            timeToShoot = 0f;
+            return;
+         }
+         else if(gameController.shootAutomatic && Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Stationary && timeToShoot >= firingRate) //Disparo Automático
+         {
+            GameObject tempProjectile = Instantiate(laserPrefab, spawnProjectilePosition.position, Quaternion.identity) as GameObject;
+            timeToShoot = 0f;
+            return;
+         }
+    }
+}
