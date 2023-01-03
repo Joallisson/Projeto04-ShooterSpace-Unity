97c0d413ef3ed1b4a74db0e1ba0edcf9e19a4056
Joallisson <joallissonnc@gmail.com>
2023-01-03 12:45:51 -0300

    162. Movimentação dos inimigos

--- Projeto 04 Shooter Space/Assets/_Game/Scripts/EnemySpawner.cs
+++ Projeto 04 Shooter Space/Assets/_Game/Scripts/EnemySpawner.cs
@@ -5,18 +5,74 @@ using UnityEngine;
 public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private GameObject enemyPrefab;
-    [SerializeField] private float spawnDelay;
+    [SerializeField] private float spawnDelay, formationDistanceScreenX, movementSpeed;
+    private float minX, maxX;
+    private bool isMoveRight;
 
     // Start is called before the first frame update
     void Start()
     {
         SpawnUntilFull();
+        Initialize();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        MoveEnemies();
+    }
+
+    private void Initialize()
+    {
+        SetRandomDirectionMovement();
+        SetMinAndMaxWidth();
+    }
+
+    private void SetRandomDirectionMovement()
+    {
+        int radomNumber = Random.Range(0, 2);
+        if(radomNumber == 0)
+        {
+            isMoveRight = false;
+        }
+        else
+        {
+            isMoveRight = true;
+        }
+    }
+
+    private void SetMinAndMaxWidth()
+    {
+        Vector2 screenDimensions = Camera.main.ScreenToWorldPoint(new Vector2(Screen.safeArea.width, Screen.safeArea.height));
+
+        minX = -screenDimensions.x;
+        maxX = screenDimensions.x;
+    }
+
+    private void MoveEnemies()
+    {
+        if(isMoveRight)
+        {
+            transform.Translate(new Vector2(movementSpeed * Time.deltaTime, 0f));
+        }
+        else
+        {
+            transform.Translate(new Vector2(-movementSpeed * Time.deltaTime, 0f));
+        }
+
+        ChangeDirection();
+    }
+
+    private void ChangeDirection()
+    {
+        if((transform.position.x - formationDistanceScreenX) < minX)
+        {
+            isMoveRight = true;
+        }
+        else if ((transform.position.x + formationDistanceScreenX) > maxX)
+        {
+            isMoveRight = false;
+        }
     }
 
     private void SpawnUntilFull()
