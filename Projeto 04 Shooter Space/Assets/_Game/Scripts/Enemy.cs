19c11cc623acedd0c33baa418a951eaf1a95f16c
Joallisson <joallissonnc@gmail.com>
2023-01-08 17:27:37 -0300

    168. Disparo dos projeteis inimigos

--- Projeto 04 Shooter Space/Assets/_Game/Scripts/Enemy.cs
+++ Projeto 04 Shooter Space/Assets/_Game/Scripts/Enemy.cs
@@ -7,18 +7,32 @@ public class Enemy : MonoBehaviour
     public int health;
     private FlashEffect flashEffect;
     private Explosion explosion;
+    [SerializeField] private GameObject projectile;
+    [SerializeField] private Transform spawnProjectilePosition;
+    [SerializeField] private float startInvokeShoot, minShootValue, maxShootValue;
 
     // Start is called before the first frame update
     void Start()
     {
         flashEffect = this.gameObject.GetComponent<FlashEffect>();
         explosion = FindObjectOfType<Explosion>();
+        InvokeProjectile();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    public void InvokeProjectile()
+    {
+        InvokeRepeating("ShootProjectile", startInvokeShoot, Random.Range(minShootValue, maxShootValue));
+    }
+
+    public void ShootProjectile()
+    {
+        GameObject tempProjectile = Instantiate(projectile, spawnProjectilePosition.position, Quaternion.identity) as GameObject;
     }
 
     private void OnTriggerEnter2D(Collider2D target)
